6ba6fb476ed429053c00e51ce572add3ab5470ff
Breno Siqueira <breno.twenty7@gmail.com>
2021-07-02 20:05:22 -0300

    dark and light theme

---
+++ frontend/src/styles/themes/dark.js
@@ -0,0 +1,9 @@
+export default {
+  title: 'dark',
+  colors: {
+    inputBackground: '#696CA0',
+    headerBackground: '#0B2161',
+    background: '#3E406F',
+    text: 'white'
+  }
+}
---
+++ frontend/src/styles/themes/light.js
@@ -0,0 +1,9 @@
+export default {
+  title: 'light',
+  colors: {
+    inputBackground: 'white',
+    headerBackground: '#01DFD7',
+    background: '#EAFAFB',
+    text: 'black'
+  }
+}
