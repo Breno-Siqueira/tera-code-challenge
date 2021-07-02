1d9cf687b6b85435c6730d1d35938d7734a2124d
Breno Siqueira <breno.twenty7@gmail.com>
2021-07-02 20:05:58 -0300

    app

---
+++ frontend/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { ThemeProvider } from 'styled-components';
+import light from './styles/themes/light';
+import dark from './styles/themes/dark';
+import GlobalStyle from './styles/global';
+import Book from './components/Book';
+
+
+function App() {
+  const [theme, setTheme] = useState(light);
+
+  const changeTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light)
+  }
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Router>
+        <Header changeTheme={changeTheme} />
+        <div>
+          <Switch>
+            <Route path="/book/:id">
+              <Book />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </ThemeProvider >
+  );
+}
+
+export default App;
